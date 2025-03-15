cd709f1cc2b11857195c0afac0317b9d12ee42a9
solankiram2023 <145072212+solankiram2023@users.noreply.github.com>
2025-03-15 19:13:10 -0400

    Exercise_3 Completed

--- Exercise_3.java
+++ Exercise_3.java
@@ -1,8 +1,5 @@
-import java.io.*; 
-  
-// Java program to implement 
 // a Singly Linked List 
-public class LinkedList { 
+public class Exercise_3 { 
   
     Node head; // head of list 
   
@@ -17,32 +14,58 @@ public class LinkedList {
         // Constructor 
         Node(int d) 
         { 
-            //Write your code here 
+            //Write your code here
+            data=d;
+            next=null; 
+            
         } 
     } 
   
     // Method to insert a new node 
-    public static LinkedList insert(LinkedList list, int data) 
+    public static Exercise_3 insert(Exercise_3 list, int data) 
     { 
         // Create a new node with given data 
-   
+        Node newNode = new Node(data);
+
         // If the Linked List is empty, 
         // then make the new node as head 
-        
-            // Else traverse till the last node 
-            // and insert the new_node there 
+        if (list.head == null){
+            list.head = newNode;
+        }
 
+         // Else traverse till the last node 
+            // and insert the new_node there 
+        else{
+            Node last = list.head;
+            while(last.next!=null){
+                last=last.next;
+            }
             // Insert the new_node at last node 
+            last.next = newNode;
+        }
         // Return the list by head 
+        return list;
+        
+
+            
+        
         
     } 
   
     // Method to print the LinkedList. 
-    public static void printList(LinkedList list) 
+    public static void printList(Exercise_3 list) 
     {  
         // Traverse through the LinkedList 
-   
+        Node currNode=list.head;
+        System.out.print("Train status:");
+        while(currNode!=null){
             // Print the data at current node 
+            System.out.print(currNode.data+"->");
+            currNode=currNode.next;
+        }
+        System.out.println("null");
+   
+            
        
             // Go to next node 
     } 
@@ -51,7 +74,7 @@ public class LinkedList {
     public static void main(String[] args) 
     { 
         /* Start with the empty list. */
-        LinkedList list = new LinkedList(); 
+        Exercise_3 list = new Exercise_3(); 
   
         // 
         // ******INSERTION****** 
